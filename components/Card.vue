92ea51000fce32f76dc1832b9ab4c1c3bc08d96d
Stephen Toms <s.mark.toms@gmail.com>
2024-09-09 01:20:14 -0400

    updated styling

--- components/Card.vue
+++ components/Card.vue
@@ -1,5 +1,19 @@
 <template>
     <NuxtLink v-if="brewery" :to="`/brewery/${brewery.id}`">
+
+    <div class="bg-muted" style="width: 100%; overflow: hidden; height: 200px;">
+        <iframe 
+            v-if="brewery.latitude && brewery.longitude"
+            :src="`https://maps.google.com/maps?q=&layer=c&cbll=${brewery.latitude},${brewery.longitude}&cbp=12,0,0,0,0&output=svembed&disableDefaultUI=true`"
+            width="100%"
+            height="700"
+            frameborder="0"
+            style="border:0; margin-top: -250px; pointer-events: none;">
+        </iframe>
+        <div v-else class="h-[200px] w-full bg-muted rounded mb-2">
+            <img src="/mug.svg" alt="Brewery" class="object-contain w-20 mx-auto mt-10 opacity-50">
+        </div>
+    </div>
     
     <div class="border border-muted relative overflow-hidden rounded-lg shadow-lg transition-transform duration-300 ease-in-out hover:shadow-xl hover:-translate-y-2">
         <a class="absolute inset-0 z-10" :href="brewery.website" rel="ugc">
@@ -15,6 +29,7 @@
     </div>
 </NuxtLink>
 <div v-else class="border border-muted relative overflow-hidden rounded-lg shadow-lg">
+    <div class="h-[200px] w-full bg-muted rounded mb-2 animate-pulse"></div>
     <div class="p-4 bg-background">
       <div class="h-6 w-3/4 bg-muted rounded mb-2 animate-pulse"></div>
       <div class="h-4 w-1/2 bg-muted rounded mb-4 animate-pulse"></div>
--- pages/brewery/[id].vue
+++ pages/brewery/[id].vue
@@ -17,29 +17,43 @@ function formatPhoneNumber(phoneNumberString) {
 </script>
 
 <template>
-  <div class="flex flex-col min-h-[100dvh] mx-auto">
-    <section class="w-full py-12 md:py-24 lg:py-32 bg-muted">
+  <div class="flex flex-col min-h-[100dvh] mx-auto bg-amber-50">
+    <section class="w-full py-12 bg-muted text-brown">
       <div class="container px-4 md:px-6 mx-auto">
         <div class="flex flex-col items-center justify-center space-y-4 text-center">
           <div class="space-y-2">
             <h1 class="text-3xl font-bold tracking-tighter sm:text-5xl">{{ data.name }}</h1>
-            <p class="max-w-[700px] text-muted-foreground md:text-lg">
+            <p class="max-w-[700px]  md:text-lg">
               {{ data.street }} {{ data.city }}, {{ data.state }} {{ data.postal_code }}
             </p>
-            <p class="mt-24 max-w-[700px] text-muted-foreground md:text-xl">
-              Contact:
-            </p>
-            <p>
-              <a :href="`tel:${data.phone}`" class="hover:underline">
-                {{ formatPhoneNumber(data.phone) }}
-              </a>
-            </p>
-            <a :href="data.website_url" target="_blank" rel="noopener noreferrer" class="hover:underline ml-2">
-              {{ data.website_url }}
+          </div>
+        </div>
+      </div>
+    </section>
+    <section class="w-full py-6 ">
+      <div class="container px-4 md:px-6 mx-auto">
+        <div class="flex flex-col items-center justify-center text-center space-y-4">
+          <h2 class="text-2xl font-semibold ">Contact Information</h2>
+          <p>
+            <a :href="`tel:${data.phone}`" class=" hover:underline">
+              {{ formatPhoneNumber(data.phone) }}
             </a>
-            <div>
-              <iframe v-if="data.latitude && data.longitude" class="w-full h-40 mt-4" :src="`https://maps.google.com/maps?q=${data.latitude},${data.longitude}&t=&z=15&ie=UTF8&iwloc=&output=embed`" />
-            </div>
+          </p>
+          <a :href="data.website_url" target="_blank" rel="noopener noreferrer" class=" hover:underline">
+            {{ data.website_url }}
+          </a>
+          <div class="w-full max-w-2xl mt-8">
+            <iframe
+              v-if="data.latitude && data.longitude"
+              class="w-full h-64 rounded-lg shadow-md mb-4"
+              :src="`https://maps.google.com/maps?q=&layer=c&cbll=${data.latitude},${data.longitude}&cbp=12,0,0,0,0&output=svembed&disableDefaultUI=true`"
+              allowfullscreen>
+            </iframe>
+            <iframe 
+              v-if="data.latitude && data.longitude" 
+              class="w-full h-64 rounded-lg shadow-md" 
+              :src="`https://maps.google.com/maps?q=${data.latitude},${data.longitude}&t=&z=15&ie=UTF8&iwloc=&output=embed`" 
+            />
           </div>
         </div>
       </div>
